a40f3ccaae9b2cc75dbe1934e01266e0e81c7f0b
linases <lina.tauraite@gmail.com>
2024-10-01 23:52:43 +0300

    build problems  with dependencies

--- Orange_HRM_Pages/PerformancePage.cs
+++ Orange_HRM_Pages/PerformancePage.cs
@@ -12,10 +12,6 @@ namespace Orange_HRM_Pages
        // private Button AddEmployee => new(By.XPath("//a[text()='Add Employee']/parent::*"));
         private Button ConfirureTab => new(ConfirureTabLocator);
         private Button KPIsTab => new(KPIsLocator);
-        private AddPKIs
-        
-
-       
 
 
         //private TextBox FirstName => new(By.XPath("//*[@name='firstName']"));
@@ -32,32 +28,31 @@ namespace Orange_HRM_Pages
 
         public void ClickConfigureTab() => ConfirureTab.ClickWhenClicable(ConfirureTabLocator);
 
-        public  Ad ClickKPIsTab()
+        public AddKPIForm ClickKPIsTab()
         {
             KPIsTab.ClickWhenClicable(KPIsLocator);
             return new AddKPIForm(_driver);
         }
-            
 
-        public void AddEmployFullName(string firstName, string middleName, string lastName)
-        {
-            FirstName.SendKeys(firstName);
-            MiddleName.SendKeys(middleName);
-            LastName.SendKeys(lastName);
-            SaveButton.Click();
-        }
-
-        //public bool IsNameDisplayedCorrectly(string name)
+        //public void AddEmployFullName(string firstName, string middleName, string lastName)
         //{
-        //    var element = EmployeeName.GetTextToBePresentInElement(EmployeeName, name);
-        //    return element.Equals(name);
+        //    FirstName.SendKeys(firstName);
+        //    MiddleName.SendKeys(middleName);
+        //    LastName.SendKeys(lastName);
+        //    SaveButton.Click();
         //}
 
-        public bool IsEmployeeListTabOpen() => IsTabOpen(EmployeeList);
-        private bool IsTabOpen(HrmWebElement element)
-        {
-            var value = element.GetAttribute("class");
-            return value.Contains("visited");
-        }
+        ////public bool IsNameDisplayedCorrectly(string name)
+        ////{
+        ////    var element = EmployeeName.GetTextToBePresentInElement(EmployeeName, name);
+        ////    return element.Equals(name);
+        ////}
+
+        //public bool IsEmployeeListTabOpen() => IsTabOpen(EmployeeList);
+        //private bool IsTabOpen(HrmWebElement element)
+        //{
+        //    var value = element.GetAttribute("class");
+        //    return value.Contains("visited");
+        //}
     }
 }
